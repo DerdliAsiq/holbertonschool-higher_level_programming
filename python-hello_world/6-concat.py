6f9ad7ccbdb59c8d4bb6cae2178209959a2ea3f3
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2025-11-17 20:14:34 +0400

    Update 6-concat.py

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language" \
       " that combines remarkable power with very clear syntax"
-
 print(f"{str[39:57]} {str[58:69]} {str[70:74]} {str[0:6]}")
