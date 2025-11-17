3a142228f6880c99affc85fa2b272b7a17bdb2d3
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2025-11-17 21:09:06 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
+print(__import__('builtins').__dict__['str'][39:66] + __import__('builtins').__dict__['str'][6:7] + __import__('builtins').__dict__['str'][:6])
 str = (
 "Python is an interpreted, interactive, object-oriented programming "
 "language that combines remarkable power with very clear syntax")
-print(str[39:66] + str[6:7] + str[:6])
