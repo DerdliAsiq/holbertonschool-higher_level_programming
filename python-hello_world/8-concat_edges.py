f4b65146fdb007a12c8612770f2dd8cc15a760c3
DerdliAsiq <120935016+DerdliAsiq@users.noreply.github.com>
2025-11-17 21:28:20 +0400

    Update 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,1 @@
-#!/usr/bin/python3
-print(__import__('builtins').__dict__['str'][39:66] + __import__('builtins').__dict__['str'][6:7] + __import__('builtins').__dict__['str'][:6])
-str = (
-"Python is an interpreted, interactive, object-oriented programming "
-"language that combines remarkable power with very clear syntax")
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
